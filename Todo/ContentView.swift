0660a6d9fb0b04aed7529e995d6597d88d2b1026
Mohamed Ali <mohamed.ali@intercom.com.eg>
2023-06-09 21:23:55 +0300

    Initial Commit

---
+++ Todo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Todo
+//
+//  Created by Mohamed Ali on 09/06/2023.
+//
+
+import SwiftUI
+import CoreData
+
+struct ContentView: View {
+    
+    var body: some View {
+        NavigationView {
+            Text("Hello World!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Todo/TodoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoApp.swift
+//  Todo
+//
+//  Created by Mohamed Ali on 09/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct TodoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
